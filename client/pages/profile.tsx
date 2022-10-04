73c570f9f65696ff4ff0cf12526ee3e73ec509b2
Racconkek <Lyuda-mirosh@mail.ru>
2022-10-04 15:04:57 +0500

    add card in profile

--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -2,24 +2,38 @@ import React, { ReactElement } from "react";
 import { observer } from "mobx-react";
 import GlobalStore from "../mobx/GlobalStore";
 import Head from "next/head";
-import styles from './profile.module.css';
+import {Card, Heading, Image, Media} from "react-bulma-components";
 
 function ProfilePage(): ReactElement {
 
   return (
-    <div className={styles.root}>
+    <>
       <Head>
         <title>Профиль</title>
       </Head>
-      <img
-        src={GlobalStore.avatar}
-        alt="..."
-      />
-      <div>
-        <h3>{GlobalStore.getFullName}</h3>
-        <h6>{GlobalStore.email}</h6>
-      </div>
-    </div>
+      <Card>
+        <Card.Content display={'flex'} flexDirection={'row'}>
+          <Media>
+            <Media.Item renderAs="figure" align="left">
+              <Image
+                size={128}
+                alt="avatar"
+                src={GlobalStore.avatar}
+                rounded
+              />
+            </Media.Item>
+            <Media.Item>
+              <Heading>
+                {GlobalStore.getFullName}
+              </Heading>
+              <Heading subtitle size={6}>
+                {GlobalStore.email}
+              </Heading>
+            </Media.Item>
+          </Media>
+        </Card.Content>
+      </Card>
+    </>
   );
 }
 
